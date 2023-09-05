74b016c4c2ee29757cd67c0fb5ec6e6f41048c15
kazi shakerodin <shakerodink@gmail.com>
2023-09-05 19:34:23 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Update from "./Components/Update";
+import Create from "./Components/Create";
+import Read from "./Components/Read";
+
+
+function App() {
+  return (
+    <div className="container">
+
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Create/>}></Route>
+          <Route path="/read" exact  element={<Read/>}></Route>
+          <Route path="/update" element={<Update />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
